bccc065dfd5675a6be0df96ae28239a8c658e6a2
Andre <andrepaesbm@gmail.com>
2022-12-14 10:27:04 -0700

    added another connect callback to give some actions to the game

--- client.js
+++ client.js
@@ -1,22 +1,27 @@
 const net = require("net");
+const { connected } = require("process");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host:'localhost',
+    host: 'localhost',
     port: 50541
   });
+
   conn.on("data", () => {
-    console.log("you ded cuz you idled");
+    console.log("you ded cuz you idled"); //output a message from the server
   });
 
   conn.on("connect", () => {
-    console.log("Successfully connected to game server");
+    console.log("Successfully connected to game server"); //Output connected if the connection is established
+    conn.write("Name: APM"); //Output the username 
   });
 
   conn.on("connect", () => {
-    conn.write(`Name: APM`);
-  });
+    const interval = setInterval(() => {
+      //conn.write("Move: up");
+    }, 50)
+  });  
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -24,4 +29,4 @@ const connect = function () {
   return conn;
 };
 
-module.exports = { connect };
+module.exports = { connect }; //exports the connect function
