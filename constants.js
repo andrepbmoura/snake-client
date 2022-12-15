b64ae13894a3a1c741ad944873dcd03c1c1a374b
Andre <andrepaesbm@gmail.com>
2022-12-15 15:14:40 -0700

    did some refactoring to the input.js file and created constants file to add some keybinds and export those to the input file

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = 'localhost';
+const PORT = 50541;
+const Up = "w";
+const Down = "s";
+const Left = "a";
+const Right = "d";
+const Msg1 = "q";
+const Msg2 = "e";
+const Msg3 = "r";
+const quit = "\u0003";
+
+module.exports = {
+  IP,
+  PORT,
+  Up,
+  Down,
+  Left,
+  Right,
+  Msg1,
+  Msg2,
+  Msg3,
+  quit
+};
--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { Up, Down, Left, Right, Msg1, Msg2, Msg3, quit } = require('./constants');
+
 let conn;
 
 // setup interface to handle user input from stdin
@@ -12,22 +14,22 @@ const setupInput = function (connection) {
 };
 
 // handles user input from stdin with the callback function
-const handleUserInput = function (key) {
-  if (key === "q") {
+const handleUserInput = function(key) {
+  if (key === Msg1) {
     conn.write("Say: LOL");
-  } else if (key === "e") {
+  } else if (key === Msg2) {
     conn.write("Say: WoW");
-  } else if (key === "r") {
+  } else if (key === Msg3) {
     conn.write("Say: -_-");
-  } else if (key === "w") {
+  } else if (key === Up) {
     conn.write("Move: up");
-  } else if (key === "a") {
-    conn.write("Move: left");
-  } else if (key === "d") {
-    conn.write("Move: right");
-  } else if (key === "s") {
+  } else if (key === Down) {
     conn.write("Move: down");
-  } else if (key === "\u0003") {
+  } else if (key === Left) {
+    conn.write("Move: left");
+  } else if (key === Right) {
+    conn.write("Move: right");
+  } else if (key === quit) {
     process.exit();
   }
 };
